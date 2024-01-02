fd50c95c28de0b5f9a1b8e3274e57fa44d9c894a
mcmah309 <dillonmcmahon79@gmail.com>
2024-01-02 05:03:27 -0500

    feat: update flake

--- flake.nix
+++ flake.nix
@@ -1,65 +1,4 @@
-# {
-#   description = "Flake to set up python environment with conda";
-
-#   inputs = {
-#       nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-#   };
-
-#   outputs = { self, nixpkgs, ... }:
-#   let
-#     system = "x86_64-linux";
-#     pkgs = import nixpkgs {
-#       inherit system;
-#     };
-#   in {
-#     devShells.x86_64-linux.default = pkgs.stdenv.mkDerivation {
-#       name = "my-shell-environment";
-#       buildInputs = [ pkgs.conda ];
-#       shellHook = ''
-#         set -e
-#         if [ ! -f ~/.conda/etc/profile.d/conda.sh ]; then
-#             mkdir -p ~/.conda/etc/profile.d/
-#             touch ~/.conda/etc/profile.d/conda.sh
-#             # "conda-shell" uses this on every shell. "conda-install" will overwrite this same file with the conda
-#             # configuration. So "conda-install" will only be wrant if this file does not exist.
-#             echo "conda-install -u" >> ~/.conda/etc/profile.d/conda.sh
-#         fi
-#         echo Entering conda shell...
-#         conda-shell
-#         echo Leaving conda shell...
-#         set +e
-#       '';
-#     };
-#   };
-# }
-
-# {
-#   description = "Flake to set up python environment with conda";
-
-#   inputs = {
-#       nixpkgs.url = "github:mcmah309/nixpkgs/master";
-#   };
-
-#   outputs = { self, nixpkgs, ... }:
-#   let
-#     system = "x86_64-linux";
-#     pkgs = import nixpkgs {
-#       inherit system;
-#     };
-#   in {
-#     devShells.x86_64-linux.default = pkgs.stdenv.mkDerivation {
-#       name = "my-shell-environment";
-#       buildInputs = [ pkgs.conda ];
-#       shellHook = ''
-#         set -e
-#         echo Entering conda shell...
-#         conda-shell
-#         echo Leaving conda shell...
-#         set +e
-#       '';
-#     };
-#   };
-# }
+# https://github.com/mcmah309/Fooocus?tab=readme-ov-file#linux-using-anaconda
 
 {
   description = "Flake to set up python environment with conda";
@@ -77,9 +16,11 @@
   in {
     devShells.x86_64-linux.default = pkgs.stdenv.mkDerivation {
       name = "my-shell-environment";
-      buildInputs = [ pkgs.conda ];
+      buildInputs = [ pkgs.conda pkgs.glib ];
       shellHook = ''
         set -e
+        export LD_LIBRARY_PATH=''${LD_LIBRARY_PATH}:${pkgs.glib.out}/lib
+
         conda_config_file="$HOME/.conda/etc/profile.d/conda.sh"
         if [ ! -f "$conda_config_file" ]; then
             echo "HELLO"
